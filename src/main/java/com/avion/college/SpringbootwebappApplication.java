4fe2d22878d2611aeaca185aa85e9e3f2a750a91
amitkv321 <amitkv321@gmail.com>
2020-03-14 15:05:10 +0530

    init repo

---
+++ src/main/java/com/avion/college/SpringbootwebappApplication.java
@@ -0,0 +1,15 @@
+package com.avion.college;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class SpringbootwebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootwebappApplication.class, args);
+	}
+
+}
